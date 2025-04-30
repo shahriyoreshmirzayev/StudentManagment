dd23d6eb3ef803e92ee0f795db3a3e8202676928
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-04-30 18:28:34 +0500

    Controllerdagi kamchilik to'g'irlandi

---
+++ StudentManagment.Client/Services/StudentService.cs
@@ -0,0 +1,49 @@
+﻿using StudentManagment.Shared.Models;
+using StudentManagment.Shared.StudentRepository;
+using System.Net.Http.Json;
+
+namespace StudentManagment.Client.Services
+{
+    public class StudentService : IStudentRepository
+    {
+        private readonly HttpClient _httpClient;
+        public StudentService(HttpClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+
+
+        public async Task<Student> AddStudentAsync(Student student)
+        {
+            var newstudent = await _httpClient.PostAsJsonAsync<Student>("api/Student/Add-Student", student);
+            var response = await newstudent.Content.ReadFromJsonAsync<Student>();
+            return response;
+        }
+
+        public Task<Student> DeleteStudentAsync(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Student>> GetAllStudentsAsync()
+        {
+            var allstudents = await _httpClient.GetAsync("api/Student/All-Students");
+            var response = await allstudents.Content.ReadFromJsonAsync<List<Student>>();
+            return response;
+        }
+
+        public async Task<Student> GetStudentByIdAsync(int studentId)
+        {
+            var singlestudent = await _httpClient.GetAsync("api/Student/Single-Student/" + studentId); // shu yerda xatolik bo'lishi mumkin
+            var response = await singlestudent.Content.ReadFromJsonAsync<Student>();
+            return response;
+        }
+
+        public async Task<Student> UpdateStudentAsync(Student student)
+        {
+            var newstudent = await _httpClient.PostAsJsonAsync<Student>("api/Student/Update-Student", student);
+            var response = await newstudent.Content.ReadFromJsonAsync<Student>();
+            return response;
+        }
+    }
+}
