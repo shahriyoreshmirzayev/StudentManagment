0b4c77500fbf7893798aa7594187c6221b508735
Shahriyor <139459691+shahriyoreshmirzayev@users.noreply.github.com>
2025-05-04 22:49:39 +0500

    ParentService qo'shildi

---
+++ StudentManagment.Client/Services/ParentService.cs
@@ -0,0 +1,33 @@
+﻿using StudentManagment.Shared.Models;
+using StudentManagment.Shared.StudentRepository;
+
+namespace StudentManagment.Client.Services
+{
+    public class ParentService : IParentRepository
+    {
+        public Task<Parent> AddAsync(Parent mod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Parent> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Parent>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Parent> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Parent> UpdateAsync(Parent mod)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
